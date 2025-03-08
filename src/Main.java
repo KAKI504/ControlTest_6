2d03f1867fe31ce9625a5d8601de9d5a157e182d
kanat <esenalievkanat.90@gmail.com>
2025-03-08 16:51:45 +0600

    Реализовал основной класс для запуска сервера планировщика

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,8 @@ public class Main {
     public static void main(String[] args) {
         try {
             int port = 9889;
-
             TaskServer server = new TaskServer("localhost", port);
             server.start();
-
             System.out.println("Сервер запущен на http://localhost:" + port + "/");
         } catch (IOException e) {
             System.err.println("Ошибка при запуске сервера: " + e.getMessage());
